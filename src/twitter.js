abb25303b76ed12c639c0c5f5515eb17935181d9
Aline Cherez <alinecherez@gmail.com>
2019-02-19 19:33:35 -0300

    Twitter versão 0.0.1.5

--- src/twitter.js
+++ src/twitter.js
@@ -4,8 +4,29 @@ let textInput = document.getElementById("textInput");
 let tweetOutput = document.getElementById("tweetOutput");
 
 function tweetPrint (){
-	let newPrint = document.createElement("p");
-	newPrint.textContent = textInput.value;
+	let newPrint = document.createElement('p');
+	newPrint.innerHTML = textInput.value;
 	tweetOutput.appendChild(newPrint);
-	textInput.value = ''
+	textInput.value = '';
 }
+
+function countDown (){
+	const tweetLenght = 140;
+	let count = tweetLenght - textInput.value.length;
+	document.getElementById('countDown').innerHTML = count;
+}
+
+// document.getElementById("tweetButton").disabled = true;
+
+// function countChar(valor) {
+//  let tweetLenght = 140;
+//  let total = valor.length;
+//  if(total < tweetLenght && total >= 0) {
+//    document.getElementById("tweetButton").disabled = false;
+//      let resto = tweetLenght - total;
+//      document.getElementById('countdown').innerHTML = resto;
+//  } else {
+//      document.getElementById('textInput').value = valor.substr(0, tweetLenght);
+//      document.getElementById("tweetButton").disabled = true;
+//  }
+// }
