b24e97548dd0387a55b7607fb778cbed90c54040
Aline Cherez <alinecherez@gmail.com>
2019-02-20 16:28:29 -0300

    Twitter versão 0.0.4

--- src/twitter.js
+++ src/twitter.js
@@ -5,11 +5,12 @@ let tweetOutput = document.getElementById("tweetOutput");
 const tweetLenght = 140;
 
 tweetButton.addEventListener('click', tweetPrint);
-textInput.addEventListener('keyup', checkButton)
+textInput.addEventListener('keyup', checkButton);
+textInput.addEventListener('keyup', pressEnterRow); 
 
-function tweetPrint (){
+function tweetPrint() {
 	let newPrint = document.createElement('p');
-	newPrint.innerHTML = textInput.value;
+	newPrint.innerHTML = textInput.value.replace(/\r?\n/g, '<br />');
 	tweetOutput.appendChild(newPrint);
 	//limpeza
 	textInput.value = '';
@@ -17,24 +18,30 @@ function tweetPrint (){
 	checkButton();
 }
 
-function countDown (){
+function countDown() {
 	let count = tweetLenght - textInput.value.length;
-		if (count <= 20 && count > 10){
-		    counter.style.color = 'orange';		
-	} else{
-		if (count <= 10){
+	if (count <= 20 && count > 10) {
+		counter.style.color = 'orange';
+	} else {
+		if (count <= 10) {
 			counter.style.color = 'red';
-		} else{
+		} else {
 			counter.style.color = 'black';
 		}
 	}
 	counter.innerHTML = count;
 }
 
-function checkButton () {
-  if (textInput.value.length <= tweetLenght && textInput.value.length > 0) {
+function checkButton() {
+	if (textInput.value.length <= tweetLenght && textInput.value.length > 0) {
 		tweetButton.removeAttribute('disabled');
-  } else {
+	} else {
 		tweetButton.setAttribute('disabled', true);
+	}
+}
+
+function pressEnterRow (event){
+  if (event.keyCode === 13) {
+    textInput.setAttribute('rows', parseInt(textInput.getAttribute('rows')) +1);
   }
 }
